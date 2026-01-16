c3e3d36857c6e9249e85e0d9deeb405baa9287f0
koko402868-cloud <koko402868@gmail.com>
2026-01-16 17:54:54 +0630

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.static("Do"));
 
+// public folder ကို serve
+app.use(express.static("public"));
+
+// button press API
 app.post("/press", (req, res) => {
   const { key } = req.body;
-
-  console.log("User pressed:", key); // 👈 Render log ထဲမှာပေါ်မယ်
-
-  res.json({ status: "ok", key });
+  console.log("User pressed:", key); // Render logs မှာပေါ်မယ်
+  res.json({ ok: true, key });
 });
 
 const PORT = process.env.PORT || 3000;
